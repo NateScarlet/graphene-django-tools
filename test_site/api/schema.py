cef9896dfec36d85e0a27a018635aa7ea49033a8
NateScarlet <NateScarlet@Gmail.com>
2018-10-12 15:50:57 +0800

    Update schema

--- test_site/api/schema.py
+++ test_site/api/schema.py
@@ -1,18 +1,28 @@
 """GrapQl schema.  """
 
 import graphene
+import graphene_django
+from django.contrib.auth.models import User
 
-import graphene_django_tools
+import graphene_django_tools as gdtools
+from graphene_django_tools import auth
 
 
 class Mutation(graphene.ObjectType):
     """Mutation """
 
-    create_user = graphene_django_tools.auth.UserCreation.Field()
-    update_user = graphene_django_tools.auth.UserUpdate.Field()
+    create_user = auth.UserCreation.Field()
+    update_user = auth.UserUpdate.Field()
+    login = auth.Login.Field()
+    logout = auth.Logout.Field()
+
+
+class Query(graphene.ObjectType):
+    """Query"""
+    user = gdtools.ModelField(auth.User)
 
 
 SCHEMA = graphene.Schema(
-    query=graphene_django_tools.utils.EmptyQuery,
+    query=Query,
     mutation=Mutation,
     auto_camelcase=False)
