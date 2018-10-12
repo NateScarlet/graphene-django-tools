d651dbf3492a2421447ddc9251fc40889c7bdff2
NateScarlet <NateScarlet@Gmail.com>
2018-10-12 16:09:46 +0800

    Add todo

--- graphene_django_tools/__about__.py
+++ graphene_django_tools/__about__.py
@@ -4,4 +4,5 @@ __author__ = 'NateScarlet@Gmail.com'
 # TODO: Add tests
 # TODO: Support other python version
 # TODO: Implement `ModelBulkUpdateMutation`
-# TODO: Sort field on nodemodel.
+# TODO: Sort field on nodemodel
+# TODO: Find documentation for MutationContext.root
