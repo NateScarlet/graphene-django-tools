5cbfcf87ce7dbb494db8838623c6f032aeac697e
NateScarlet <NateScarlet@Gmail.com>
2018-10-12 16:03:19 +0800

    Implement `ModelConnectionField` `ModelFilterConnectionField`

--- graphene_django_tools/__init__.py
+++ graphene_django_tools/__init__.py
@@ -1,8 +1,10 @@
 
 """Addtional tools for `graphene_django`.  """
 
+from graphql.execution.base import ResolveInfo
+
 from . import utils
 from .core import create_modelnode, get_modelnode
 from .mutation import (ClientIDMutation, ModelCreationMutaion, ModelMutaion,
                        ModelUpdateMutaion, Mutation, MutationContext)
-from .types import ModelField
+from .types import ModelConnectionField, ModelField, ModelFilterConnectionField
--- graphene_django_tools/types.py
+++ graphene_django_tools/types.py
@@ -1,16 +1,45 @@
 """Graphene types.  """
 import graphene
+import graphene_django
+import graphene_django.filter
 
 from . import core
 
 
 class ModelField(graphene.Field):
-    """Field for mapping a model.
+    """`Field` for model.
 
-    User create_nodemodel first if you want custom the field.
+    Use `create_nodemodel` first if you want customize the node.
     """
 
     def __init__(self, model, **kwargs):
         kwargs.setdefault('description',
-                          model.__doc__ or f'Node for database model: {model.__name__}')
+                          (model.__doc__
+                           or f'Node for database model: {model.__name__}'))
+        super().__init__(core.get_modelnode(model), **kwargs)
+
+
+class ModelConnectionField(graphene_django.DjangoConnectionField):
+    """`DjangoConnectionField` for model.
+
+    Use `create_nodemodel` first if you want customize the node.
+    """
+
+    def __init__(self, model, **kwargs):
+        kwargs.setdefault('description',
+                          (model.__doc__
+                           or f'Connection for database model: {model.__name__}'))
+        super().__init__(core.get_modelnode(model), **kwargs)
+
+
+class ModelFilterConnectionField(graphene_django.filter.DjangoFilterConnectionField):
+    """`DjangoFilterConnectionField` for model.
+
+    Use `create_nodemodel` first if you want customize the node.
+    """
+
+    def __init__(self, model, **kwargs):
+        kwargs.setdefault('description',
+                          (model.__doc__
+                           or f'Fitlerable connection for database model: {model.__name__}'))
         super().__init__(core.get_modelnode(model), **kwargs)
