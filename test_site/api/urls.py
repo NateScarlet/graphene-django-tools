06e8a99b891290e95bff1c6515d1e717d24a88ac
NateScarlet <NateScarlet@Gmail.com>
2018-10-12 17:07:11 +0800

    Add csrf_exempt for test_site

--- test_site/api/urls.py
+++ test_site/api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from .schema import SCHEMA
 
 app_name = 'api'
 urlpatterns = [
-    path('', GraphQLView.as_view(graphiql=True, schema=SCHEMA))
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=SCHEMA)))
 ]
