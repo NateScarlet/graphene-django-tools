4af83ea7c8d6b9cfa9e7ee6122d722ecb965f0f6
NateScarlet <NateScarlet@Gmail.com>
2018-10-12 15:49:42 +0800

    Remove auto import for `auth`

--- graphene_django_tools/__init__.py
+++ graphene_django_tools/__init__.py
@@ -1,7 +1,8 @@
 
 """Addtional tools for `graphene_django`.  """
 
-from . import auth, utils
+from . import utils
+from .core import create_modelnode, get_modelnode
 from .mutation import (ClientIDMutation, ModelCreationMutaion, ModelMutaion,
                        ModelUpdateMutaion, Mutation, MutationContext)
 from .types import ModelField
