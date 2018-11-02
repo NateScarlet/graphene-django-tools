ba886ea50ccb85ec4a8cb3b7858cff15f2e8c1d3
NateScarlet <NateScarlet@Gmail.com>
2018-11-02 18:38:04 +0800

    Update TODO

--- graphene_django_tools/__about__.py
+++ graphene_django_tools/__about__.py
@@ -1,6 +1,7 @@
 __version__ = '0.10.0'
 __author__ = 'NateScarlet@Gmail.com'
 
+# TODO: Handle usecase that query_name difference with resolve_name
 # TODO: Add tests
 # TODO: Support other python version
 # TODO: Sort field on nodemodel
