9dd4c1b03f3d7e20cc35074dbd6b2d0cfb789f8f
NateScarlet <NateScarlet@Gmail.com>
2018-12-18 17:53:09 +0800

    Refactor `ModelFilterConnectionField`

--- graphene_django_tools/types.py
+++ graphene_django_tools/types.py
@@ -4,8 +4,8 @@ from functools import partial
 
 import graphene
 import graphene_django
-import graphene_django.filter
 import isodate
+from graphene_django.filter import DjangoFilterConnectionField
 from graphql.language import ast
 from promise import Promise
 
@@ -114,16 +114,26 @@ class ModelConnectionField(CustomConnectionResolveMixin, graphene_django.DjangoC
         return on_resolve(iterable)
 
 
-class ModelFilterConnectionField(CustomConnectionResolveMixin,
-                                 graphene_django.filter.DjangoFilterConnectionField):
+class ModelFilterConnectionField(ModelConnectionField):
     """`DjangoFilterConnectionField` for model.  """
+
+    args = DjangoFilterConnectionField.args
+    filterset_class = DjangoFilterConnectionField.filterset_class
+    filtering_args = DjangoFilterConnectionField.filtering_args
+    merge_querysets = DjangoFilterConnectionField.merge_querysets
+    get_resolver = DjangoFilterConnectionField.get_resolver
 
     def __init__(self, model, **kwargs):
         kwargs.setdefault('description',
                           (model.__doc__
                            or f'Fitlerable connection for database model: {model.__name__}'))
         self._node_filterset_class = None
-        super().__init__(lambda: core.get_modelnode(model), **kwargs)
+        self._fields = None
+        self._provided_filterset_class = None
+        self._filterset_class = None
+        self._extra_filter_meta = None
+        self._base_args = None
+        super().__init__(model, **kwargs)
 
     @property
     def _provided_filterset_class(self):
@@ -135,6 +145,38 @@ class ModelFilterConnectionField(CustomConnectionResolveMixin,
     @_provided_filterset_class.setter
     def _provided_filterset_class(self, value):
         self._node_filterset_class = value
+
+    @classmethod
+    def connection_resolver(
+            cls,
+            resolver,
+            connection,
+            default_manager,
+            max_limit,
+            enforce_first_or_last,
+            filterset_class,
+            filtering_args,
+            root,
+            info,
+            **kwargs):
+        # pylint: disable=R0913,W0221
+        filter_kwargs = {k: v for k,
+                         v in kwargs.items()
+                         if k in filtering_args}
+        queryset = filterset_class(
+            data=filter_kwargs,
+            queryset=default_manager.get_queryset(),
+            request=info.context,).qs
+
+        return super().connection_resolver(
+            resolver,
+            connection,
+            queryset,
+            max_limit,
+            enforce_first_or_last,
+            root,
+            info,
+            **kwargs)
 
 
 class Duration(graphene.Scalar):
