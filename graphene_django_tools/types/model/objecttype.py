816c239d9b7687f61d467ff279398d7eddb579ec
NateScarlet <NateScarlet@Gmail.com>
2019-09-12 11:11:47 +0800

    fix: ModelNode should pass `filterset_class` when `graphene_django` >= 2.4.0

--- graphene_django_tools/types/model/objecttype.py
+++ graphene_django_tools/types/model/objecttype.py
@@ -17,5 +17,8 @@ class ModelNode(graphene_django.DjangoObjectType):
         # pylint: disable=W0221
         _meta = options.get('_meta', core.ModelNodeOptions(cls))
         _meta.filterset_class = filterset_class
+        if hasattr(graphene_django.types.DjangoObjectTypeOptions, 'filterset_class'):
+            # This attribute added in `graphene_django-2.4.0`
+            options['filterset_class'] = filterset_class
         options['_meta'] = _meta
         super().__init_subclass_with_meta__(**options)
