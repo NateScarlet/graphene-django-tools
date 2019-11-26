19fafb0f84c9388fc8fc13e0f01f4f4a81e72361
NateScarlet <NateScarlet@Gmail.com>
2019-11-26 13:56:43 +0800

    feat: allow use string as data loader key

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: test dev
 
-dist: graphene_django_tools pyproject.toml
+dist: graphene_django_tools/* graphene_django_tools/*/* graphene_django_tools/*/*/* pyproject.toml
 	poetry build
 
 dev:
--- graphene_django_tools/dataloader/middleware.py
+++ graphene_django_tools/dataloader/middleware.py
@@ -14,6 +14,7 @@ def get_model_batch_load_fn(model):
     """Create batch load function for model.  """
 
     def batch_load_fn(keys):
+        keys = [int(i) for i in keys]
         LOGGER.debug('load: %s: %s', model, keys)
         result = model.objects.in_bulk(keys)
         return Promise.resolve([result[i] for i in keys])
